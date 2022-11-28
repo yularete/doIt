97ec8ae4ac94d5e4f31cfd26bcde58ea50319e46
yularete <keinikeiwo@gmail.com>
2022-11-28 13:23:00 +0900

    15. 자바 입출력 / FileInputStreamTest / 우왕 예외처리가 된다고 프로그램이 중단되거나 멈추지 않는균... 역시 예외 처리는 중요하네

---
+++ doItJAVAhhhhh/src/stream/inputstream/FileInputStreamTest1.java
@@ -0,0 +1,29 @@
+package stream.inputstream;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+
+public class FileInputStreamTest1 {
+    public static void main(String[] args) {
+
+        FileInputStream fis = null;
+
+        try{
+            fis = new FileInputStream("input.txt"); //input.txt 파일 입력 스트림 생성
+            System.out.println(fis.read());
+            System.out.println(fis.read());
+            System.out.println(fis.read());
+        } catch (IOException e) {
+            System.out.println(e);
+        } finally {
+            try {
+                fis.close(); //열린 스트림은 finally 블록에서 닫음
+            } catch (IOException e) {
+                System.out.println(e);
+            } catch (NullPointerException e) {
+                System.out.println(e);
+            }
+        }
+        System.out.println("end");
+    }
+}
