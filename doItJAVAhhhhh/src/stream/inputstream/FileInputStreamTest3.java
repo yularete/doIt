40a582db45d4dfb35d0a0e8802bce00fad4a9e7d
yularete <keinikeiwo@gmail.com>
2022-11-28 15:24:14 +0900

    15. 자바 입출력 / FileInputStreamTest3 / 메서드를 사용할 때는 매개변수뿐 아니라 반환 값의 의미도 잘 확인해야 한다.

---
+++ doItJAVAhhhhh/src/stream/inputstream/FileInputStreamTest3.java
@@ -0,0 +1,25 @@
+package stream.inputstream;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+public class FileInputStreamTest3 {
+    public static void main(String[] args) {
+        try (FileInputStream fis = new FileInputStream("input2.txt")){
+            byte[] bs = new byte[10];
+            int i;
+            while ((i = fis.read(bs)) != -1) {
+                for (int k = 0; k<i; k++) {
+                    System.out.print((char)bs[k]);
+                }
+                System.out.println(": " + i + "바이트 읽음");
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        System.out.println("end");
+    }
+}
