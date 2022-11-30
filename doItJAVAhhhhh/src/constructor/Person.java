8b96b4984fb8cc6231947bfcdab9f484a75b3c2f
yularete <keinikeiwo@gmail.com>
2022-11-30 13:31:14 +0900

    15. 클래스와 객체 1 / 생성자를 만들고 테스트해보기

---
+++ doItJAVAhhhhh/src/constructor/Person.java
@@ -0,0 +1,7 @@
+package constructor;
+
+public class Person {
+    String name;
+    float height;
+    float weight;
+}
---
+++ doItJAVAhhhhh/src/constructor/PersonTest.java
@@ -0,0 +1,7 @@
+package constructor;
+
+public class PersonTest {
+    public static void main(String[] args) {
+        Person personBam = new Person(); // Person() <- 생성자
+    }
+}
