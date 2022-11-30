aa860b4ca3fcd20474f22b8e462d47e108cbc39b
yularete <keinikeiwo@gmail.com>
2022-11-30 15:46:00 +0900

    6. 클래스와 객체|| / 생성자에서 다른 생성자를 호출할 때 this를 사용한다

---
+++ doItJAVAhhhhh/src/thisex/CallAnotherConst.java
@@ -0,0 +1,23 @@
+package thisex;
+
+class Person{
+    String name;
+    int age;
+
+
+    Person() {
+        this("이름 없음", 1); //this를 사용해 Person(String, int)생성자 호출
+    }
+
+    Person(String name, int age){
+    this.name = name;
+    this.age = age;
+    }
+}
+public class CallAnotherConst {
+    public static void main(String[] args) {
+        Person noName = new Person();
+        System.out.println(noName.name);
+        System.out.println(noName.age);
+    }
+}
