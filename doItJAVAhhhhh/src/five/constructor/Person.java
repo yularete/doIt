0ccbd4cde344c957abd34f2d5182ee27f8ca2298
yularete <keinikeiwo@gmail.com>
2022-11-30 15:38:00 +0900

    6. 클래스와 객체 II / 자신의 메모리를 가리키는 this

--- doItJAVAhhhhh/src/five/constructor/Person.java
+++ doItJAVAhhhhh/src/five/constructor/Person.java
@@ -8,7 +8,7 @@ public class Person {
     public Person() {} //디폴트 생성자
 
     public Person(String name){ //이름을 매개변수로 입력받는 생성자
-        name = pname;
+//        name = pname;
     }
     public Person(String pname, float pheight, float pweight) {
         name = pname;
