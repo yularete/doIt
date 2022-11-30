807474d3c0cc9f917259fb055f1c77229c560176
yularete <keinikeiwo@gmail.com>
2022-11-30 15:51:49 +0900

    6. 클래스와 객체 || / 클래스 자료형과 상관없이 클래스 내에서 this를 사용하면 자신의 주소 값을 반환할 수 있다.

--- doItJAVAhhhhh/src/thisex/CallAnotherConst.java
+++ doItJAVAhhhhh/src/thisex/CallAnotherConst.java
@@ -13,11 +13,20 @@ class Person{
     this.name = name;
     this.age = age;
     }
+
+    Person returnItSelf(){
+        return this; //this 반환
+    }
+
 }
 public class CallAnotherConst {
     public static void main(String[] args) {
         Person noName = new Person();
         System.out.println(noName.name);
         System.out.println(noName.age);
+
+        Person p = noName.returnItSelf(); // this 값을 클래스 변수에 대입
+        System.out.println(p);          //noName.returnItSelf()의 반환 값 출력
+        System.out.println(noName);     //참조 변수 출력
     }
 }
