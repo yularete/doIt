e6548df81b998b91cbcfb0d8a0dc999fa44a9341
yularete <keinikeiwo@gmail.com>
2022-11-30 13:11:20 +0900

    5. 클래스와 객체 1 / 클래스 사용과 main()함수

---
+++ doItJAVAhhhhh/src/classpart/StudentTest.java
@@ -0,0 +1,11 @@
+package classpart;
+
+public class StudentTest {
+    public static void main(String[] args) {
+        Student studentBam = new Student();// Student 클래스 생성
+        studentBam.studentName = "밤브리";
+
+        System.out.println(studentBam.studentName);
+        System.out.println(studentBam.getStudentName());
+    }
+}
---
+++ doItJAVAhhhhh/src/classpart/StudentTest1.java
@@ -0,0 +1,13 @@
+package classpart;
+
+public class StudentTest1 {
+    public static void main(String[] args) {
+        Student student1 = new Student();// 첫 번째 학생 생성
+        student1.studentName = "밤브리";
+        System.out.println(student1.getStudentName());
+
+        Student student2 = new Student();// 두 번째 학생 생성
+        student2.studentName = "오렌지";
+        System.out.println(student2.getStudentName());
+    }
+}
