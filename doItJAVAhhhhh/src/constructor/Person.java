efa547e248bdf978a821e3099f6a7642a01dcb92
yularete <keinikeiwo@gmail.com>
2022-11-30 13:44:47 +0900

    15. 클래스와 객체1 / 디폴트 생성자와 매개변수가 있는 생성자 테스트

--- doItJAVAhhhhh/src/constructor/Person.java
+++ doItJAVAhhhhh/src/constructor/Person.java
@@ -4,4 +4,15 @@ public class Person {
     String name;
     float height;
     float weight;
+
+    public Person() {} //디폴트 생성자
+
+    public Person(String name){ //이름을 매개변수로 입력받는 생성자
+        name = pname;
+    }
+    public Person(String pname, float pheight, float pweight) {
+        name = pname;
+        height = pheight;
+        weight = pweight;
+    }
 }
--- doItJAVAhhhhh/src/constructor/PersonTest.java
+++ doItJAVAhhhhh/src/constructor/PersonTest.java
@@ -3,5 +3,10 @@ package constructor;
 public class PersonTest {
     public static void main(String[] args) {
         Person personBam = new Person(); // Person() <- 생성자
+        personBam.name ="밤브리";
+        personBam.weight = 750F;
+        personBam.height = 9F;
+
+        Person personO = new Person("오렌지",4,45);
     }
 }
