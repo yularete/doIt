81817ebce00f6648d20fd3123b5436cf9f077171
yularete <keinikeiwo@gmail.com>
2022-12-03 23:48:19 +0900

    6. 클래스와 객체|| / 클래스 이름으로 static 변수 참조하기

---
+++ doItJAVAhhhhh/src/staticex/StudentTest3.java
@@ -0,0 +1,18 @@
+package staticex;
+
+public class StudentTest3 {
+    public static void main(String[] args) {
+        Student1 studentBam = new Student1();
+        studentBam.setStudentName("밤브리");
+        System.out.println(studentBam.serialNum);
+        System.out.println(Student1.serialNum);
+        System.out.println(studentBam.studentName + " 학번:"+studentBam.studentID);
+
+        Student1 studentO = new Student1();
+        studentO.setStudentName("오렌지");
+        System.out.println(studentO.serialNum);
+        System.out.println(Student1.serialNum);
+        System.out.println(studentO.studentName + " 학번:"+studentBam.studentID);
+
+    }
+}
