729b2020a507debcd6d98a5de4401449dde39280
yularete <keinikeiwo@gmail.com>
2022-12-03 23:56:28 +0900

    6.클래스와 객체 || / public 메서드 만들어서 외부에서 참조할 수 있게 하기

--- doItJAVAhhhhh/src/singleton/Company.java
+++ doItJAVAhhhhh/src/singleton/Company.java
@@ -3,4 +3,11 @@ package singleton;
 public class Company {
     private static Company instance = new Company();
     private Company(){}
+
+    public static Company getInstance() {
+        if(instance == null) {
+            instance = new Company();
+        }
+        return instance;
     }
+}
