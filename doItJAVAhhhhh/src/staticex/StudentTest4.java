3e271bb3e8ce85e7830b664ce85267b8d5b4da81
yularete <keinikeiwo@gmail.com>
2022-12-03 23:51:22 +0900

    6. 클래스와 객체|| / get(), set() 메서드로 학번 출력하기

---
+++ doItJAVAhhhhh/src/staticex/StudentTest4.java
@@ -0,0 +1,16 @@
+package staticex;
+
+public class StudentTest4 {
+    public static void main(String[] args) {
+        Student1 studentBam = new Student1();
+        studentBam.setStudentName("밤브리");
+        System.out.println(Student2.getSerialNum());
+        System.out.println(studentBam.studentName + " 학번:"+studentBam.studentID);
+
+        Student1 studentO = new Student1();
+        studentO.setStudentName("오렌지");
+        System.out.println(Student2.getSerialNum());
+        System.out.println(studentO.studentName + " 학번:"+studentBam.studentID);
+
+    }
+}
