4d8dd39cb7f925f01f4b0f2e82222ef3372bf5ce
yularete <keinikeiwo@gmail.com>
2022-12-05 23:50:23 +0900

    8. 상속과 다형성 / super 예약어로 매개변수가 있는 생성자를 호출할 수 있게 VIPCustomer 클래스 생성자 추가

--- doItJAVAhhhhh/src/JAVA_LAB/chapter8/inheritance/VIPCustomer.java
+++ doItJAVAhhhhh/src/JAVA_LAB/chapter8/inheritance/VIPCustomer.java
@@ -4,12 +4,20 @@ public class VIPCustomer extends Customer {
     private int agentID;
     double saleRatio;
 
-    public VIPCustomer(){
+//    public VIPCustomer(){
+//        customerGrade = "VIP";  //상위 클래스에서 private 변수이므로 오류 발생
+//        bonusRatio = 0.05;
+//        saleRatio = 0.1;
+//        System.out.println("VIPCustomer() 생성자 호출");
+//    }
+    public VIPCustomer(int customerID, String customerName, int agentID){
+        super(customerID, customerName);
         customerGrade = "VIP";  //상위 클래스에서 private 변수이므로 오류 발생
         bonusRatio = 0.05;
         saleRatio = 0.1;
-        System.out.println("VIPCustomer() 생성자 호출");
-    }
+        this.agentID = agentID;
+        System.out.println("VIPCustomer(int, String) 생성자 호출");
+}
     public int getAgentID(){
         return agentID;
     }
