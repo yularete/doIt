45a77d24b89df69a4b9758c966dc82a97d2eb36d
yularete <keinikeiwo@gmail.com>
2022-12-05 23:38:29 +0900

    8. 상송과 다향성 / protected 예약어로 선언한 변수를 외부에서 사용할 수 있도록 get(), set() 메서드 추가

--- doItJAVAhhhhh/src/JAVA_LAB/chapter8/inheritance/Customer.java
+++ doItJAVAhhhhh/src/JAVA_LAB/chapter8/inheritance/Customer.java
@@ -2,9 +2,9 @@ package JAVA_LAB.chapter8.inheritance;
 
 public class Customer {
 
-    private int customerID;
-    private String customerName;
-    private String customerGrade;
+    protected int customerID;
+    protected String customerName;
+    protected String customerGrade;
     int bonusPoint;
     double bonusRatio;
 
@@ -18,5 +18,23 @@ public class Customer {
     }
     public String showCustomerInfo() {
         return customerName + "님의 등급은 " + customerGrade + "이며, 보너스 포인트는" + bonusPoint +"입니다.";
+    }
+    public int getCustomerID(){         //protected 예약어로 선언한 변수를 외부에서 사용할 수 있도록 get(), set() 메서드 추가
+        return customerID;
+    }
+    public void setCustomerID(int customerID) {
+        this.customerID = customerID;
+    }
+    public String getCustomerName(){
+        return customerName;
+    }
+    public void setCustomerName(String customerName){
+        this.customerName = customerName;
+    }
+    public String getCustomerGrade(){
+        return customerGrade;
+    }
+    public void setCustomerGrade(String customerGrade){
+        this.customerGrade = customerGrade;
     }
 }
